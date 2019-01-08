459389dd21cd5ea2542a854593c58667c0b0f329
Kaitlin Mahar <kaitlinmahar@gmail.com>
2019-01-08 12:52:32 -0500

    add task env var

--- travis_install.sh
+++ travis_install.sh
@@ -1,5 +1,5 @@
 # no install is needed if we are simply linting.
-if [[ "$LINT" == "TRUE" ]]; then exit 0; fi
+if [[ "$TASK" == "LINT" ]]; then exit 0; fi
 
 MONGODB_BASE="mongodb-linux-x86_64"
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then MONGODB_BASE="mongodb-osx-ssl-x86_64"; fi
@@ -9,9 +9,6 @@ wget http://fastdl.mongodb.org/${TRAVIS_OS_NAME}/${MONGODB_BASE}-${MONGODB_VERSI
 mkdir mongodb-${MONGODB_VERSION}
 tar xzvf ${MONGODB_BASE}-${MONGODB_VERSION}.tgz -C mongodb-${MONGODB_VERSION} --strip-components 1
 ${PWD}/mongodb-${MONGODB_VERSION}/bin/mongod --version
-
-# install swiftenv
-#eval "$(curl -sL https://swiftenv.fuller.li/install.sh)"
   
 # clone and build libmongoc
 git clone -b r1.13 https://github.com/mongodb/mongo-c-driver /tmp/libmongoc
